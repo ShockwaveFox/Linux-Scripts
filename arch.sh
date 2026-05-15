a6ccc53d01f4d8d418bc2dca2294a15eca431216
Laces <55741347+ShockwaveFox@users.noreply.github.com>
2026-05-15 12:16:05 +0100

    Add files via upload
    Added arch file

---
+++ arch.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# list of packages to install
+arch_packages=(steam discord goverlay mangohud lib32-mangohud gamemode lib32-gamemode vulkan-radeon lib32-vulkan-radeon vulkan-tools mesa pavucontrol)
+aur_packages=(heroic-games-launcher-bin protonplus)
+security_packages=(apparmor ufw clamav)
+
+# sets script to stop at any errors, undefined variables or piping failures
+set -euo pipefail
+
+# prints message to screen
+echo "Updating system"
+
+# update system
+sudo pacman -Syu
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "Installing Arch packages"
+
+# installs Arch packages
+sudo pacman -S --needed "${arch_packages[@]}"
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "Installing AUR packages"
+
+# installs AUR packages
+yay -S --needed "${aur_packages[@]}"
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "Installing Security packages"
+
+sudo pacman -S --needed "${security_packages[@]}"
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "Enabling Security packages"
+
+# enable security packages
+sudo systemctl enable --now ufw
+sudo systemctl enable --now apparmor
+sudo systemctl enable --now clamav-freshclam
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "Setting Firewall rules"
+
+# set ufw rules
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
+# prints message to screen
+echo "\n===========================================\n"
+
+# Prints message to screen
+echo "installation complete"
